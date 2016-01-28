0f06f156849a2b738e86f4c51502493c00c20085
Arne Coucheron <arco68@gmail.com>
2016-01-29 00:18:10 +0100

    Sync media codecs with LA.AF.1.2.1_rb1.5
    Change-Id: Id71f4bc783461236938bbf59562eceed2fbca5fa

--- msm8660.mk
+++ msm8660.mk
@@ -212,6 +212,8 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_codecs_google_performance.xml:system/etc/media_codecs_google_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
