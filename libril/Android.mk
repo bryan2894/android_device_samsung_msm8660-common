468b29b8a56399f92461be86c1cc586ebc3c435d
Martin Bouchet <tincho5588@gmail.com>
2017-09-23 04:54:37 -0300

    ril: import Oreo AOSP libril
    Change-Id: I00750bad812dc263e9c74a504b48512a892376c8

--- libril/Android.mk
+++ libril/Android.mk
@@ -1,31 +1,45 @@
 # Copyright 2006 The Android Open Source Project
 
-ifeq ($(BOARD_PROVIDES_LIBRIL),true)
+ifneq ($(BOARD_PROVIDES_LIBRIL),true)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_VENDOR_MODULE := true
+
 LOCAL_SRC_FILES:= \
     ril.cpp \
     ril_event.cpp\
-    RilSocket.cpp \
     RilSapSocket.cpp \
+    ril_service.cpp \
+    sap_service.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils \
-    libbinder \
     libcutils \
     libhardware_legacy \
     librilutils \
+    android.hardware.radio@1.0 \
+    android.hardware.radio.deprecated@1.0 \
+    libhidlbase  \
+    libhidltransport \
+    libhwbinder
 
 LOCAL_STATIC_LIBRARIES := \
     libprotobuf-c-nano-enable_malloc \
 
+<<<<<<< HEAD:libril/Android.mk
 LOCAL_CFLAGS :=
 
 ifeq ($(SIM_COUNT), 2)
     LOCAL_CFLAGS += -DANDROID_MULTI_SIM
+=======
+LOCAL_CFLAGS += -Wno-unused-parameter
+
+ifeq ($(SIM_COUNT), 2)
+    LOCAL_CFLAGS += -DANDROID_MULTI_SIM -DDSDA_RILD1
+>>>>>>> afc35718... ril: import Oreo AOSP libril:ril/libril/Android.mk
     LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
 endif
 
@@ -43,6 +57,7 @@ endif
 
 include $(BUILD_SHARED_LIBRARY)
 
+<<<<<<< HEAD:libril/Android.mk
 
 # For RdoServD which needs a static library
 # =========================================
@@ -62,4 +77,6 @@ LOCAL_MODULE:= libril_static
 
 include $(BUILD_STATIC_LIBRARY)
 endif # ANDROID_BIONIC_TRANSITION
+=======
+>>>>>>> afc35718... ril: import Oreo AOSP libril:ril/libril/Android.mk
 endif # BOARD_PROVIDES_LIBRIL
